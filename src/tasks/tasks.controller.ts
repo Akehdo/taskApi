809a49b875474bcdc134568e7d8163375278e912
Ruslan Kadirov <ruslankadirov1337@gmail.com>
2025-04-06 23:23:16 +0500

    Initial commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dtos/create-task-dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  getAll() {
+    return this.tasksService.getAll();
+  }
+
+  @Get(":id")
+  getById(@Param('id') id: string) {
+    return this.tasksService.getById(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreateTaskDto) {
+    return this.tasksService.create(dto);
+  }
+
+  @Delete(":id")
+  delete(@Param('id') id: string) {
+    return this.tasksService.delete(id);
+  }
+}
